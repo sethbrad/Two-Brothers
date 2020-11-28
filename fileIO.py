a469646d4a1dc7430fb4a003f52f702a36631242
Seth Braddock <sethbrad@iastate.edu>
2020-11-28 16:40:11 -0600

    commenting files

--- fileIO.py
+++ fileIO.py
@@ -1,8 +1,12 @@
+"""We import libraries to use functions that the Python team has made for us"""
 import array
 
+"""This opens a text file in write mode and declares an array. 
+An array is simply a list of values, in this case of integers"""
 f = open("file.txt", "w")
 arr = array.array('i', [1,2,3,4,5])
 
+"""Now we're going to iterate through the array so each value is written to the file"""
 for i in range(0, len(arr)):
     f.write(str(arr[i]) + " ")
 
---
+++ pyDemo.py
@@ -0,0 +1,23 @@
+"""Python is an interpreted language. 
+This means that is is not compiled but instead converted directly into machine code (binary)"""
+
+"""Comments are ignored by the intepreter and wrapped in triple quotes like this"""
+
+"""We use variables to store values"""
+number = 7
+word = "John"
+
+"""All we need is a name and a value separated by the '=' sign. Each line is distinct"""
+"""We can output strings to the console with the following syntax"""
+print ("Hello, world!")
+
+"""We can also do arithmetic on number variables"""
+x = 4
+y = 3
+z = x + y
+
+print (str(z))
+
+"""Numbers can be compared as well, which gives a boolean value --> true or false"""
+print (x < y)
+print (z > x)
