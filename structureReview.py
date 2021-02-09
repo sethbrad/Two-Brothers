bc74af2ed6dcc457c2be97d41f707567cc05c812
Seth Braddock <sethbrad@iastate.edu>
2021-02-09 10:57:24 -0600

    structure file

---
+++ structureReview.py
@@ -0,0 +1,28 @@
+### basic data types
+phrase = "this is a string"
+char = 'z'  #character
+num1 = 4    #integer
+num2 = 3.14     #float
+state = True    #boolean
+
+# if statement
+if(state):
+    # takes a boolean, then executes code inside
+    print("if passed!")
+else:
+    # executes when argument is false
+    print("if failed!")
+
+# while loop
+count = 5
+
+while (count > 0):
+    print(count)
+    count -= 1
+
+# for loop
+for i in range(0, 5):
+    print(i)
+
+
+
