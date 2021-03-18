facbf6610e771d07af0b6f5d9b1d2d3a8712b1d2
Seth Braddock <sethbrad@iastate.edu>
2021-03-17 23:14:53 -0500

    drip bitch

---
+++ loopsExample.py
@@ -0,0 +1,12 @@
+for i in range(0, 10):
+    print(i)
+
+###################################
+
+counter = 9
+while (counter >= 0):
+    print(counter)
+    counter = counter - 1 # counter -= 1 OR counter--
+
+
+# Git -> add to stage, commit stage, push
